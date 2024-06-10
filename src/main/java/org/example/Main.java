516a4c177854cd0362fad66791ff9bdb44db674e
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2024-06-10 19:44:18 -0400

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import io.gatling.javaapi.core.Simulation;
+
+public class Main extends Simulation {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
